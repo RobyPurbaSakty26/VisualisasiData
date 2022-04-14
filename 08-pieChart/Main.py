c582bc919466dae78e45a42650f3e9b33e8b3f8e
RobyPurbaSakty26 <obykao26@gmail.com>
2022-04-14 23:01:43 +0700

    pie

--- 08-pieChart/Main.py
+++ 08-pieChart/Main.py
@@ -1,7 +1,13 @@
+from multiprocessing.sharedctypes import Value
+from optparse import Values
+from typing import ValuesView
 import matplotlib.pyplot as plt
 import numpy as np
 
 y = np.array([35, 25, 25, 15])
 
-plt.pie(y)
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+
+plt.pie(y, labels=mylabels)
 plt.show() 
